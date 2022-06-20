adfffd44c2be04546806f7e28b63da98e75504bd
dave <dave@olympus>
2022-06-19 22:40:06 -0500

    Update conf for edited pics

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Sigal static site thing
+
+Builds with some python script I found called sigal
+
+Uses github to deploy to azure
+
+Sigal is located: `~/.local/bin`
--- sigal.conf.py
+++ sigal.conf.py
@@ -13,7 +13,7 @@
 
 # Source directory. Can be set here or as the first argument of the `sigal
 # build` command
-source = '/mnt/j/pictures/Marina Party'
+source = '/mnt/c/Users/dave/OneDrive/Pictures/Camera imports/2022-06-19'
 
 # Destination directory. Can be set here or as the second argument of the
 # `sigal build` command (default: '_build')
